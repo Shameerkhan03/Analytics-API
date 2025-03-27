1a6a0cb54e2f96104053ba8fef36c135ee305145
shameer <shameerrr03@gmail.com>
2025-03-27 06:44:48 +0500

    added configuration for timescaledb in compose.yaml

--- app/api/events/routing.py
+++ app/api/events/routing.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -5,4 +6,5 @@ router = APIRouter()
 
 @router.get("/")
 def read_events():
+    print(os.environ.get("DATABASE_URL"))
     return {"items": [1, 2, 3]}
